3c2c2d34671dc972b1cc79bb07322bed60d70b7d
anamarianovais <marianaximenesnovais@gmail.com>
2024-11-29 00:33:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem foi Napoleão Bonaparte?',
+    'Napoleão Bonaparte foi um líder militar e imperador da França no início do século XIX'
+);
+
+criaCartao(
+    'Ciências',
+    'O que são células-tronco?',
+    'Células-tronco são células capazes de se transformar em vários tipos de células do corpo'
+);
+
+criaCartao(
+    'Matemática',
+    'O que é o Teorema de Pitágoras?',
+    'O Teorema de Pitágoras estabelece a relação entre os lados de um triângulo retângulo'
+);
+
+criaCartao(
+    'Arte',
+    'O que é o movimento impressionista?',
+    'O impressionismo é um movimento artístico que valoriza a luz e a cor, capturando momentos'
+);
